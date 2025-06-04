9151d843d5f4085697784f7bb91872a7ebc8597f
rickme86 <rickmerks.design@gmail.com>
2025-06-04 12:42:11 +0200

    Update main.js

--- main.js
+++ main.js
@@ -56,7 +56,7 @@ function setParallaxStrengthForAll(meshes, strength) {
   });
 }
 
-const FIXED_PARALLAX_STRENGTH = isTouchDevice ? 0.08 : 0.09;
+const FIXED_PARALLAX_STRENGTH = isTouchDevice ? 0.08 : 0.5;
 
 
 init();
